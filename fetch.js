9ccc07f72355515f77440155cf39607476797c90
Brayan Gutiérrez <78226844+Brayan-Gutierrez@users.noreply.github.com>
2024-01-07 16:58:39 +0000

    finish

--- fetch.js
+++ fetch.js
@@ -1,3 +1,34 @@
+fetch(url, {
+  method: 'POST', // GET, POST, PUT, PATCH, DELETE
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer acadebieseexistiruntokendeautorizacion'
+  },
+  body: JSON.stringify({
+    name:'Chanchito feliz',
+    website: 'google.com'
+  })
+})
+.then((response) => response.json())
+.then(data => console.log(data))
+
+
+//Usndo async await
 const fn = async () => {
-  const response = await fetch ()
+  const response = await fetch (url, {
+    method: 'POST', //GET, POST, PUT, PATCH, DELETE
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer acadebieseexistiruntokendeautorizacion'
+    },
+    body: JSON.stringify(
+      {
+        name: 'Chanchito feliz',
+        website: 'google.com'
+      }
+    )
+  })
+  const data  = await response.json()
+  console.log(data);
 }
+fn()
